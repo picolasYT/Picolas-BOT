8d4b11540a10755bc71daea69a68c31636885b80
picolasYT <nicolasguevarakraan@gmail.com>
2025-11-18 15:25:06 -0300

    msg

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('Picolas BOT', {
+say('Yuki Suou', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
 })
-say('Made with love by Picolas', {
+say('Made with love by Destroy', {
 font: 'console',
 align: 'center',
 colors: ['cyan', 'magenta', 'yellow']
